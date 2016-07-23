e60900824ec9e50014b4ff2fc4cd0ec6dcabb22b
Garrett Smith <garrettwhsmith@gmail.com>
2016-07-23 14:40:13 -0500

    sample data graph

--- src/browser/tracking/Chart.react.js
+++ src/browser/tracking/Chart.react.js
@@ -9,6 +9,11 @@ import {
 } from 'react-onsenui';
 import {prop} from 'ramda';
 import moment from 'moment';
+import {
+  VictoryLine,
+  VictoryAxis,
+  VictoryScatter,
+} from 'victory';
 
 export default class Chart extends Component {
 
@@ -23,30 +28,25 @@ export default class Chart extends Component {
   render() {
     const data = [
       {
-        name: 'Physical',
-        values: [
-          {
-            createdAt: moment().toDate(),
-            val: 2,
-          },
-          {
-            createdAt: moment().subtract(1, 'd').toDate(),
-            val: 3,
-          },
-          {
-            createdAt: moment().subtract(2, 'd').toDate(),
-            val: 5,
-          },
-          {
-            createdAt: moment().subtract(3, 'd').toDate(),
-            val: 4,
-          },
-          {
-            createdAt: moment().subtract(4, 'd').toDate(),
-            val: 2,
-          },
-        ],
-      }
+        createdAt: moment().toDate(),
+        val: 2,
+      },
+      {
+        createdAt: moment().subtract(1, 'd').toDate(),
+        val: 3,
+      },
+      {
+        createdAt: moment().subtract(2, 'd').toDate(),
+        val: 5,
+      },
+      {
+        createdAt: moment().subtract(3, 'd').toDate(),
+        val: 4,
+      },
+      {
+        createdAt: moment().subtract(4, 'd').toDate(),
+        val: 2,
+      },
     ];
 
     const domain = {
@@ -56,10 +56,40 @@ export default class Chart extends Component {
       ],
       y: [0, 5],
     };
-    console.log(data, domain)
 
     return (
       <Page className="chart">
+        <svg width="100%" height="100%">
+          <VictoryLine data={data}
+            domain={domain}
+            interpolation="cardinal"
+            x="createdAt"
+            y="val"
+            standAlone={false}
+          />
+
+          <VictoryScatter
+            data={data}
+            domain={domain}
+            x="createdAt"
+            y="val"
+            standAlone={false}
+            labels={prop('val')}
+          />
+
+          <VictoryAxis
+            standAlone={false}
+            domain={domain.x}
+            tickFormat={x => moment(x).format('ddd D')}
+          />
+
+          <VictoryAxis
+            dependentAxis
+            standAlone={false}
+            domain={domain.y}
+            tickFormat={x => `${x}`}
+          />
+        </svg>
       </Page>
     )
   }
